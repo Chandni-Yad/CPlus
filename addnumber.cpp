8db2c8b0444c521df2b20fe21874272cce11fb38
Chandni-Yad <chandniyadav904419@gmail.com>
2024-09-05 14:35:50 +0530

    Create addnumber.cpp

---
+++ addnumber.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+  // sum of numbers from 1 t0 10
+int sum = 0;
+  for (int i=0; i<= 10; i++){
+    sum = sum+i;
+  }
+  cout << sum << endl;
+
+  return 0;
+}
